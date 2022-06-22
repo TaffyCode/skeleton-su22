354c9887c9cac9d5c8a75fafc5d31a2774207a3d
Raphael Pelayo <raffie@berkeley.edu>
2022-06-21 21:19:35 -0700

    Finished LeapYear

--- lab01/LeapYear.java
+++ lab01/LeapYear.java
@@ -1,6 +1,6 @@
 /**
  * Class that determines whether or not a year is a leap year.
- * @author YOUR NAME HERE
+ * @author Raphael Pelayo
  */
 public class LeapYear {
 
@@ -9,7 +9,10 @@ public class LeapYear {
      * @source CS 61BL Lab 1
      */
     public static boolean isLeapYear(int year) {
-        // TODO: Fill in this method.
+        if (year % 400 == 0 || ((year % 4 == 0) && (year % 100 != 0))) {
+            return true;
+        }
+        return false;
     }
 
     /** Calls isLeapYear to print correct statement. */
