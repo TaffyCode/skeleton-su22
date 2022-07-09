f74f3f534f81e846708f3dd60755a931b512f730
Raphael Pelayo <raffie@berkeley.edu>
2022-07-09 16:57:38 -0700

    a lotta stuff

---
+++ proj2/gitlet/Main.java
@@ -0,0 +1,25 @@
+package gitlet;
+
+/** Driver class for Gitlet, a subset of the Git version-control system.
+ *  @author Raphael Pelayo
+ */
+public class Main {
+
+    /** Usage: java gitlet.Main ARGS, where ARGS contains
+     *  <COMMAND> <OPERAND1> <OPERAND2> ... 
+     */
+    Repository repository = new Repository();
+    public static void main(String[] args) {
+        // TODO: what if args is empty?
+        String firstArg = args[0];
+        switch(firstArg) {
+            case "init":
+                // TODO: handle the `init` command
+                break;
+            case "add":
+                // TODO: handle the `add [filename]` command
+                break;
+            // TODO: FILL THE REST IN
+        }
+    }
+}
